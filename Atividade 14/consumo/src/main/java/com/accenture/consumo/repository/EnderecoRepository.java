925b29015b4bc0789b5b4bf228c5eca37a514b69
AndreSFonsec <49457060+AndreSFonsec@users.noreply.github.com>
2025-01-25 12:31:15 -0300

    Add files via upload

---
+++ Atividade 14/consumo/src/main/java/com/accenture/consumo/repository/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.accenture.consumo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.accenture.consumo.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+
+}
